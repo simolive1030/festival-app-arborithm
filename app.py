3458c99cca8f5a095bc51d2b58d68dac6836a83f
Simone <s311817@studenti.polito.it>
2026-04-21 14:58:37 +0200

    Update

--- app.py
+++ app.py
@@ -493,4 +493,9 @@ def dettaglio_evento(id):
     return render_template("evento.html", evento=evento)
 
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
